4260d935fbc4ec78c40b554aab4bc7b31e2d6ca3
Raychel B <sylverpyro@gmail.com>
2016-06-18 20:56:00 -0500

    Minor change

--- retroarch/goodarchive.sh
+++ retroarch/goodarchive.sh
@@ -29,7 +29,7 @@ name="${file%.*}"
 ext="${file##*.}"
 mime="$(file "$path" --mime-type -b)"
 
-search_for="\.cso\|\.iso\|\.img\|\.bin\|\.mdf\|\.ecm\|\.nes\|\.nds\|\.nrg\|\.cdi\|\.dsk\|\.1a\|\.chf\|\.gbc\|\.gcm\|\.u26\|\.rom\|\.a78\|\.xex\|\.d64\|\.t64\|\.adf\|\.int\|\.pce\|\.gb\|\.smc\|\.gg\|\.sf7\|\.gen\|\.k7\|\.atr\|\.ssd\|\.do\|\.fmem1\|\.a52\|\.lnx\|\.st\|\.mtx\|\.sad\|\.app\|\.32x\|\.sms\|\.dim\|\.p\|\.tzx\|\.k7\|\.uef\|\.cpr\|\.jag\|\.j64\|\.a26\|\.bas\|\.atx\|\.com\|\.cas\|\.fpl\|\.fpt\|\.n64\|\.unf\|\.wad\|\.sfc\|\.3ds\|\.ws\|\.wsc\|\.col\|\.tap\|\.g64\|\.crt\|\.u[345]\|\.ue1\|\.ud7\|\.vec\|\.jrc\|\.sdf\|\.fds\|\.vb\|\.gdi\|\.sg\|\.md\|\.z80\|\.trd\|\.g41\|\.mv\|\.sc\|\.scl\|\.sp\|\.slt\|\.csw\|\.\$b\|\.fdi\|\.ngp\|\.ngc\|\.svi\|\.udi\|\.m5"
+search_for="\.cso\|\.iso\|\.img\|\.bin\|\.mdf\|\.ecm\|\.nes\|\.nds\|\.nrg\|\.cdi\|\.dsk\|\.1a\|\.chf\|\.gbc\|\.gcm\|\.u26\|\.rom\|\.a78\|\.xex\|\.d64\|\.t64\|\.adf\|\.int\|\.pce\|\.gb\|\.smc\|\.gg\|\.sf7\|\.gen\|\.k7\|\.atr\|\.ssd\|\.do\|\.fmem1\|\.a52\|\.lnx\|\.st\|\.mtx\|\.sad\|\.app\|\.32x\|\.sms\|\.dim\|\.p\|\.tzx\|\.k7\|\.uef\|\.cpr\|\.jag\|\.j64\|\.a26\|\.bas\|\.atx\|\.com\|\.cas\|\.fpl\|\.fpt\|\.n64\|\.unf\|\.wad\|\.sfc\|\.3ds\|\.ws\|\.wsc\|\.col\|\.tap\|\.g64\|\.crt\|\.u[345]\|\.ue1\|\.ud7\|\.vec\|\.jrc\|\.sdf\|\.fds\|\.vb\|\.gdi\|\.sg\|\.md\|\.z80\|\.trd\|\.g41\|\.mv\|\.sc\|\.scl\|\.sp\|\.slt\|\.csw\|\.\$b\|\.fdi\|\.ngp\|\.ngc\|\.svi\|\.udi\|\.m5\|\.sv\|\.tgc"
 
 count=""
 case "$mime" in
